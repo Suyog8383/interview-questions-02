e5338de033d6ca7ec8182cb0c37c8303ec56c845
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-05-09 18:44:05 +0530

    crystaltechnolab company questions

---
+++ questions.js
@@ -0,0 +1,83 @@
+// let a = 10;
+
+// {
+//   var a = 20;
+// }
+
+// console.log(a)
+
+// let x = {
+//     name :"Akash",
+//     print: () => {
+//         console.log(this.name)
+//     }
+// }
+
+
+
+// let y = {
+//     name :"Akash",
+//     print: function () {
+//         console.log(this.name)
+//     }
+// }
+
+// x.print()
+// y.print()
+
+// function add(item, items = []) {
+//   items.push(item);
+//   return items;
+// }
+
+// console.log(add("Orange"));
+
+// console.log(add("Apple"));
+
+
+// function outer(f = inner()) {
+//   function inner() {
+//     return "Inner";
+//   }
+// }
+// outer();
+
+// function myFun(x, y, ...manyMoreArgs) {
+//   console.log(manyMoreArgs);
+// }
+
+// myFun(1, 2, 3, 4, 5);
+// myFun(1, 2);
+
+// const obj = { key: "value" };
+
+// const array = [...obj];
+
+// console.log(array);
+
+// console.log(typeof typeof typeof true);
+
+// let count = 10;
+
+// function innerFunc() {
+   
+//   if (count === 10) {
+//     let count = 11;
+//     console.log(count);
+//   }
+  
+//   console.log(count);
+// };
+
+// innerFunc();
+
+
+// (function innerFunc() {
+    
+//   if (count === 10) {
+//     let count = 11;
+//     console.log(count);
+//   }
+  
+//   console.log(count);
+// })();
